743f6b66857dcf3f7c69ebac3a00af7adf033925
Dominik <dominik.rubroeder@icloud.com>
2022-05-13 20:12:05 +0200

    Add media assets and set favicon

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,16 @@ export default function Home() {
     <div>
       <Head>
         <title>Space tourism</title>
-        <meta name="description" content="So, you want to travel to space" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Space tourism | So, you want to travel to space"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/assets/favicon-32x32.png"
+          sizes="32x32"
+        ></link>
       </Head>
 
       <header>Header</header>
